9d55c1681dc3f841dad4ce79e5fcea42dc46bc12
Rob Leonard <leonarr1@tcd.ie>
2020-02-17 13:00:52 +0000

    stub

---
+++ CVA-app/components/GovtCommitments/GovtCommitmentsHome.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import styles from '../../assets/styleSheet'
+
+
+const GovtCommitmentsHome = ({navigation}) => {
+  return (
+                  <Text style={styles.heading} >
+                      Govt Commitments Home Screen
+                  </Text>
+  );
+}
+
+export default GovtCommitmentsHome;
--- CVA-app/routes/homeStack.js
+++ CVA-app/routes/homeStack.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../components/Home';
 import ActionPlanHome from '../components/ActionPlans/ActionPlanHome'
+import GovtCommitmentsHome from '../components/GovtCommitments/GovtCommitmentsHome'
 
 const Stack = createStackNavigator();
 
@@ -13,6 +14,7 @@ function MyStack() {
             <Stack.Navigator>
                 <Stack.Screen name="Home" component={Home} />
                 <Stack.Screen name="Action-Plan" component={ActionPlanHome} />
+                <Stack.Screen name="Govt-Commitments" component={GovtCommitmentsHome} />
             </Stack.Navigator>
       </NavigationContainer>
     );
