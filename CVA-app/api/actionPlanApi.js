32f37f6299eaf457b1af4f7df71c09f16b49da34
Rob Leonard <leonarr1@tcd.ie>
2020-02-19 17:00:21 +0000

    added comment explanation

--- CVA-app/api/actionPlanApi.js
+++ CVA-app/api/actionPlanApi.js
@@ -2,37 +2,42 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 export function createAction(action, createComplete) {
-    firebase.firestore().collection('actionPlans').add({
-        title: action.title,
-        description: action.description,
-        carriedOutBy: action.carriedOutBy,
-        monitoredBy: action.monitoredBy,
-        comments: action.comments,
-        deadline: action.deadline,
-        creationDate: firebase.firestore.FieldValue.serverTimestamp()
-    }).then((data) => data.get()
-    ).then((actionData) => createComplete(actionData.data()))
-        .catch((error) => console.log(error));
+  firebase.firestore().collection('actionPlans').add({
+    title: action.title,
+    description: action.description,
+    carriedOutBy: action.carriedOutBy,
+    monitoredBy: action.monitoredBy,
+    comments: action.comments,
+    deadline: action.deadline,
+    creationDate: firebase.firestore.FieldValue.serverTimestamp()
+  }).then((data) => data.get()
+  ).then((actionData) => createComplete(actionData.data()))
+  .catch((error) => console.log(error));
 }
 
 export async function getActions(actionsFetched) {
-    var actionPlans = [];
-    var data = await firebase.firestore().collection('actionPlans').orderBy('creationDate').get()
+  var actionPlans = [];
+  var data = await firebase.firestore()
+  .collection('actionPlans')
+  .orderBy('creationDate')
+  .get()
 
-    data.forEach((document) => {
-        let temp = document.data();
-        const actionPlan = {
-            id: document.id,
-            title: temp.title,
-            description: temp.description,
-            carriedOutBy: temp.carriedOutBy,
-            monitoredBy: temp.monitoredBy,
-            comments: temp.comments,
-            deadline: temp.deadline,
-            creationDate: temp.creationDate
-        }
-        actionPlans.push(actionPlan)
-    });
+  // Formating to include the doc id in the object
+  // might be a better way of doing this.
+  data.forEach((document) => {
+    let temp = document.data();
+    const actionPlan = {
+      id: document.id,
+      title: temp.title,
+      description: temp.description,
+      carriedOutBy: temp.carriedOutBy,
+      monitoredBy: temp.monitoredBy,
+      comments: temp.comments,
+      deadline: temp.deadline,
+      creationDate: temp.creationDate
+    }
+    actionPlans.push(actionPlan)
+  });
 
   actionsFetched(actionPlans);
 }
