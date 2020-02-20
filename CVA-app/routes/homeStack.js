e3ccccce2553e2846a41be0764a4703ee19950d4
Arthur9817 <sasuntsa@tcd.ie>
2020-02-20 22:19:49 +0000

    Report screen/functionality added

--- CVA-app/routes/homeStack.js
+++ CVA-app/routes/homeStack.js
@@ -6,47 +6,46 @@ import Home from "../components/Home";
 import ActionPlanHome from "../components/ActionPlans/ActionPlanHome";
 import GovtCommitmentsHome from "../components/GovtCommitments/GovtCommitmentsHome";
 import Commitment from "../components/GovtCommitments/Commitment";
-import CreateCommitment from '../components/GovtCommitments/CreateCommitment';
+import CreateCommitment from "../components/GovtCommitments/CreateCommitment";
 import ActionPlan from "../components/ActionPlans/ActionPlan";
+import Report from "../components/Reports/Report";
 
 const Stack = createStackNavigator();
 
 function MyStack() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="Action-Plan"
-          component={ActionPlanHome}
-          options={{ title: "Action Plans" }}
-        />
-        <Stack.Screen
-          name="ActionPlan"
-          component={ActionPlan}
-          options={({ route }) => ({
-            title: route.params.param?.title
-          })}
-        />
-        <Stack.Screen
-          name="Govt-Commitments-Home"
-          component={GovtCommitmentsHome}
-          options={{ title: "Government Commitments" }}
-        />
-        <Stack.Screen
-          name="Commitment"
-          component={Commitment}
-          options={({ route }) => ({
-            title: route.params.details?.commitmentTitle
-          })}
-        />
-        <Stack.Screen
-          name="CreateCommitment"
-          component={CreateCommitment}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen
+					name="Action-Plan"
+					component={ActionPlanHome}
+					options={{ title: "Action Plans" }}
+				/>
+				<Stack.Screen
+					name="ActionPlan"
+					component={ActionPlan}
+					options={({ route }) => ({
+						title: route.params.param?.title
+					})}
+				/>
+				<Stack.Screen
+					name="Govt-Commitments-Home"
+					component={GovtCommitmentsHome}
+					options={{ title: "Government Commitments" }}
+				/>
+				<Stack.Screen
+					name="Commitment"
+					component={Commitment}
+					options={({ route }) => ({
+						title: route.params.details?.commitmentTitle
+					})}
+				/>
+				<Stack.Screen name="CreateCommitment" component={CreateCommitment} />
+				<Stack.Screen name="Report" component={Report} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
 export default MyStack;
