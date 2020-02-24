0c4452fc99757c19d833b0ab29d7125d3fcf4203
Rob Leonard <leonarr1@tcd.ie>
2020-02-24 18:04:41 +0000

    refresh needed of screen

--- CVA-app/components/GovtCommitments/Commitment.js
+++ CVA-app/components/GovtCommitments/Commitment.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import { View, ScrollView, TouchableOpacity, Text } from "react-native";
 import { Table, Row, Rows } from "react-native-table-component";
 import styles from "../../assets/styleSheet";
 import CreateCommitment from "../GovtCommitments/CreateCommitment";
@@ -30,40 +30,42 @@ function Commitment({ navigation, route }) {
 	}
 
 	return (
-		<View>
-			<View style={styles.standardsTableOuterContainer}>
-				<View style={styles.standardsTableInnerContainer}>
-					<Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
-						<Row data={state.tableHead} />
-						<Rows data={state.standards} />
-					</Table>
-				</View>
-			</View>
-			<View style={{ flexDirection: "row", justifyContent: "space-around" }}>
-				<TouchableOpacity style={styles.buttonContainer}>
-					<Text>View Actual Standards</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					style={styles.buttonContainer}
-					onPress={() => {
-						navigation.navigate("Report", { passParams });
-					}}
-				>
-					<Text>Create Standards Report</Text>
-				</TouchableOpacity>
-			</View>
-			<TouchableOpacity
-				style={styles.buttonContainer}
-				onPress={() => { handleDeleteCommitment(navigation); }}>
-				<Text>Delete Commitment</Text>
-			</TouchableOpacity>
-			<TouchableOpacity
-				style={styles.buttonContainer}
-				onPress={() => { handleUpdateCommitment(); }}>
-				<Text>Edit Commitment</Text>
-			</TouchableOpacity>
-			<CreateCommitment props={passParams}/>
-		</View>
+		<ScrollView>
+			{state.renderEditor ? <CreateCommitment props={passParams} /> :
+				<View>
+					<View style={styles.standardsTableOuterContainer}>
+						<View style={styles.standardsTableInnerContainer}>
+							<Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
+								<Row data={state.tableHead} />
+								<Rows data={state.standards} />
+							</Table>
+						</View>
+					</View>
+					<View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+						<TouchableOpacity style={styles.buttonContainer}>
+							<Text>View Actual Standards</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							style={styles.buttonContainer}
+							onPress={() => {
+								navigation.navigate("Report", { passParams });
+							}}
+						>
+							<Text>Create Standards Report</Text>
+						</TouchableOpacity>
+					</View>
+					<TouchableOpacity
+						style={styles.buttonContainer}
+						onPress={() => { handleDeleteCommitment(navigation); }}>
+						<Text>Delete Commitment</Text>
+					</TouchableOpacity>
+					<TouchableOpacity
+						style={styles.buttonContainer}
+						onPress={() => { handleUpdateCommitment(state); }}>
+						<Text>Edit Commitment</Text>
+					</TouchableOpacity>
+				</View>}
+		</ScrollView>
 	);
 }
 
@@ -72,8 +74,10 @@ const handleDeleteCommitment = (navigation) => {
 	navigation.goBack();
 };
 
-const handleUpdateCommitment = () => {
-
+const handleUpdateCommitment = (state) => {
+	console.log(state)
+	state.renderEditor = true;
+	console.log(state.renderEditor)
 }
 
 export default Commitment;
