66ba26754989dfe661c7cbf70c68cbbc0bf1a7f2
Vahe Sasunts <sasuntsv@tcd.ie>
2020-03-10 16:04:03 +0000

    removed warnings coming up for presenting

--- CVA-app/components/Home.js
+++ CVA-app/components/Home.js
@@ -1,30 +1,39 @@
-import * as React from 'react';
-import firebase from 'firebase'
-import { View, Text, StyleSheet,TouchableOpacity, Button } from 'react-native';
-import styles from '../assets/styleSheet'
+import * as React from "react";
+import firebase from "firebase";
+import { View, Text, StyleSheet, TouchableOpacity, Button } from "react-native";
+import styles from "../assets/styleSheet";
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
+  console.disableYellowBox = true;
   return (
-          <View style={styles.container}>
-              <View style={styles.container} >
-                  <Text style={styles.heading} >
-                      Home Page To be designed and styled..
-                  </Text>
-                  <TouchableOpacity style={styles.buttonContainer} onPress={()=>navigation.navigate('Action-Plan')}>
-                    <Text style={ styles.buttonText} >Action Plan</Text> 
-                  </TouchableOpacity>
-                  <View style={{paddingTop:20}}></View>
-                  <TouchableOpacity style={styles.buttonContainer} onPress={()=>navigation.navigate('Govt-Commitments-Home')}>
-                    <Text style={ styles.buttonText} >Govt Commitments</Text> 
-                  </TouchableOpacity>
-                  <View style={{paddingTop:20}}></View>
-                 <TouchableOpacity style={styles.buttonContainer} onPress={()=> firebase.auth().signOut()} >
-                      <Text style={ styles.buttonText} >Logout</Text>
-                  </TouchableOpacity>
-              </View>
-          
-          </View>
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Home Page To be designed and styled..
+        </Text>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => navigation.navigate("Action-Plan")}
+        >
+          <Text style={styles.buttonText}>Action Plan</Text>
+        </TouchableOpacity>
+        <View style={{ paddingTop: 20 }}></View>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => navigation.navigate("Govt-Commitments-Home")}
+        >
+          <Text style={styles.buttonText}>Govt Commitments</Text>
+        </TouchableOpacity>
+        <View style={{ paddingTop: 20 }}></View>
+        <TouchableOpacity
+          style={styles.buttonContainer}
+          onPress={() => firebase.auth().signOut()}
+        >
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
-}
+};
 
 export default Home;
