6a4e01a984481e877bcb532e6e148bf2744e7ba7
Vahe Sasunts <sasuntsv@tcd.ie>
2020-03-22 12:50:57 +0000

    fixed merge issue

--- CVA-app/App.js
+++ CVA-app/App.js
@@ -1,24 +1,22 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import firebase from 'firebase'
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import firebase from "firebase";
 import { getUser } from "./api/profileApi";
-import Loading from './components/Loading';
-import LoginForm from './components/LoginScreen/LoginForm';
-import Navigator from './routes/homeStack';
-import OnBoard from './routes/OnBoardingStack';
+import Loading from "./components/Loading";
+import LoginForm from "./components/LoginScreen/LoginForm";
+import Navigator from "./routes/homeStack";
+import OnBoard from "./routes/OnBoardingStack";
 console.disableYellowBox = true;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-<<<<<<< HEAD
     this.state = {
       loggedIn: null,
       firstLogin: false,
       userInfo: []
-    }
-
+    };
   }
 
   onActionsFetched = userInfo => {
@@ -26,33 +24,17 @@ class App extends Component {
       userInfo: (prevState.userInfo = userInfo)
     }));
   };
-=======
-    componentDidMount() {
-        var firebaseConfig = {
-            // apiKey: "AIzaSyBgq7pJF8_D2kdE8UAdTlKQAgfbOOCXG7E",
-            // authDomain: "tomscva.firebaseapp.com",
-            // databaseURL: "https://tomscva.firebaseio.com",
-            // projectId: "tomscva",
-            // storageBucket: "tomscva.appspot.com",
-            // messagingSenderId: "944453411118",
-            // appId: "1:944453411118:web:6e8b7c6ba48c0a60ef9abf",
-            // measurementId: "G-0FDDH4XPXG"
-            apiKey: "AIzaSyBED4E6DHNaXml61ENPAvOcyZUMSd-ePeU",
-            authDomain: "cva-worldvision.firebaseapp.com",
-            databaseURL: "https://cva-worldvision.firebaseio.com",
-            projectId: "cva-worldvision",
-            storageBucket: "cva-worldvision.appspot.com",
-            messagingSenderId: "615471052636",
-            appId: "1:615471052636:web:9df5e821e9981b4fc47399"
-        };
-        // Initialize Firebase
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }
->>>>>>> govermentTracking
 
   componentDidMount() {
     var firebaseConfig = {
+      // apiKey: "AIzaSyBgq7pJF8_D2kdE8UAdTlKQAgfbOOCXG7E",
+      // authDomain: "tomscva.firebaseapp.com",
+      // databaseURL: "https://tomscva.firebaseio.com",
+      // projectId: "tomscva",
+      // storageBucket: "tomscva.appspot.com",
+      // messagingSenderId: "944453411118",
+      // appId: "1:944453411118:web:6e8b7c6ba48c0a60ef9abf",
+      // measurementId: "G-0FDDH4XPXG"
       apiKey: "AIzaSyBED4E6DHNaXml61ENPAvOcyZUMSd-ePeU",
       authDomain: "cva-worldvision.firebaseapp.com",
       databaseURL: "https://cva-worldvision.firebaseio.com",
@@ -68,40 +50,38 @@ class App extends Component {
 
     firebase.auth().onAuthStateChanged(async user => {
       if (user) {
-        await getUser(user.email, this.onActionsFetched)
+        await getUser(user.email, this.onActionsFetched);
         this.setState({
           loggedIn: true
-        })
+        });
       } else {
         this.setState({
           loggedIn: false
-        })
+        });
       }
-    })
+    });
   }
 
   renderContent = () => {
     switch (this.state.loggedIn) {
       case false:
-        return <LoginForm />
+        return <LoginForm />;
       case true:
-        console.log(this.state.userInfo)
+        console.log(this.state.userInfo);
         if (this.state.firstLogin == this.state.userInfo[0].signedUp) {
-
-          return <OnBoard />
+          return <OnBoard />;
         } else {
-          return <Navigator />
+          return <Navigator />;
         }
 
       default:
-        return <Loading />
+        return <Loading />;
     }
-  }
+  };
 
   render() {
-    return (this.renderContent()
-    );
+    return this.renderContent();
   }
-};
+}
 
 export default App;
