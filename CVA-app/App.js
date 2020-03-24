40dc9b00192c91cef634fda78ac5a1cbf7ddd10e
Arthur9817 <sasuntsa@tcd.ie>
2020-03-24 18:13:55 +0000

    Fixed navigation when first signed in

--- CVA-app/App.js
+++ CVA-app/App.js
@@ -1,82 +1,63 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import firebase from 'firebase'
-import { getUser } from "./api/profileApi";
-import Loading from './components/Loading';
-import LoginForm from './components/LoginScreen/LoginForm';
-import Navigator from './routes/homeStack';
-import OnBoard from './routes/OnBoardingStack';
+import "react-native-gesture-handler";
+import React, { Component } from "react";
+import firebase from "firebase";
+import Loading from "./components/Loading";
+import LoginForm from "./components/LoginScreen/LoginForm";
+import Navigator from "./routes/homeStack";
 console.disableYellowBox = true;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      loggedIn: null,
-      firstLogin: false,
-      userInfo: []
-    }
+		this.state = {
+			loggedIn: null
+		};
+	}
 
-  }
+	componentDidMount() {
+		var firebaseConfig = {
+			apiKey: "AIzaSyBED4E6DHNaXml61ENPAvOcyZUMSd-ePeU",
+			authDomain: "cva-worldvision.firebaseapp.com",
+			databaseURL: "https://cva-worldvision.firebaseio.com",
+			projectId: "cva-worldvision",
+			storageBucket: "cva-worldvision.appspot.com",
+			messagingSenderId: "615471052636",
+			appId: "1:615471052636:web:9df5e821e9981b4fc47399"
+		};
+		// Initialize Firebase
+		if (!firebase.apps.length) {
+			firebase.initializeApp(firebaseConfig);
+		}
 
-  onActionsFetched = userInfo => {
-    this.setState(prevState => ({
-      userInfo: (prevState.userInfo = userInfo)
-    }));
-  };
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.setState({
+					loggedIn: true
+				});
+			} else {
+				this.setState({
+					loggedIn: false
+				});
+			}
+		});
+	}
 
-  componentDidMount() {
-    var firebaseConfig = {
-      apiKey: "AIzaSyBED4E6DHNaXml61ENPAvOcyZUMSd-ePeU",
-      authDomain: "cva-worldvision.firebaseapp.com",
-      databaseURL: "https://cva-worldvision.firebaseio.com",
-      projectId: "cva-worldvision",
-      storageBucket: "cva-worldvision.appspot.com",
-      messagingSenderId: "615471052636",
-      appId: "1:615471052636:web:9df5e821e9981b4fc47399"
-    };
-    // Initialize Firebase
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    }
+	renderContent = () => {
+		switch (this.state.loggedIn) {
+			case false:
+				return <LoginForm />;
+			case true:
+				return <Navigator />;
 
-    firebase.auth().onAuthStateChanged(async user => {
-      if (user) {
-        await getUser(user.email, this.onActionsFetched)
-        this.setState({
-          loggedIn: true
-        })
-      } else {
-        this.setState({
-          loggedIn: false
-        })
-      }
-    })
-  }
+			default:
+				return <Loading />;
+		}
+	};
 
-  renderContent = () => {
-    switch (this.state.loggedIn) {
-      case false:
-        return <LoginForm />
-      case true:
-        console.log(this.state.userInfo)
-        if (this.state.firstLogin == this.state.userInfo[0].signedUp) {
-
-          return <OnBoard />
-        } else {
-          return <Navigator />
-        }
-
-      default:
-        return <Loading />
-    }
-  }
-
-  render() {
-    return (this.renderContent()
-    );
-  }
-};
+	render() {
+		return this.renderContent();
+	}
+}
 
 export default App;
